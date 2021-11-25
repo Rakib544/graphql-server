6722f30448a9f5e64c882f69da234f0c82a3c173
Rakib544 <md.rakib10122003@gmail.com>
2021-11-25 21:16:22 +0600

    first query using graphql from mongodb done

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
+
+//
 const { MONGO } = require("./config");
+const Post = require("./models/Post");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello world",
+    async getPosts() {
+      try {
+        const posts = await Post.find({});
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 

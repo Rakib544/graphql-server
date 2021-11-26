cb2ad3e4672eec680eae1e77fa8d6c2f8ba9f48f
Rakib544 <md.rakib10122003@gmail.com>
2021-11-26 22:52:54 +0600

    mutation created

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -48,5 +48,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: Id, body: String): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;

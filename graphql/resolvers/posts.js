8393512341439af062247bd4df735f3d30fdd697
Rakib544 <md.rakib10122003@gmail.com>
2021-11-27 00:42:56 +0600

    like api created

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -67,6 +67,23 @@ module.exports = {
     // like post
     async likePost(_, { postId }, context) {
       const { username } = checkauth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
     },
   },
 };

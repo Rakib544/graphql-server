aa58fcf5269fe0c3f8ab4e25c21da2cbd5d8bef2
Rakib544 <md.rakib10122003@gmail.com>
2021-11-26 12:54:14 +0600

    user registration completed

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,8 +1,12 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const User = require("../../models/User");
+const { SECRET_KEY } = require("../../config");
 
 module.exports = {
   Mutation: {
-    register(
+    async register(
       _,
       { registerInput: { username, email, password, confirmPassword } },
       context,
@@ -11,6 +15,33 @@ module.exports = {
       // TODO Validate the data
       // TODO Make sure user does not already exists
       // TODO Has password and create auth token
+
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };

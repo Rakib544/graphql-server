f049dfffc1055ac0cd11473feaa225ff774ba942
Rakib544 <md.rakib10122003@gmail.com>
2021-11-26 19:30:02 +0600

    new comit

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -10,5 +10,18 @@ module.exports = {
         throw new Error(err);
       }
     },
+
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not Found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };

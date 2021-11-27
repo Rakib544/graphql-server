d246a082708bd8b99ffeabf0636dac6b4ff76210
Rakib544 <md.rakib10122003@gmail.com>
2021-11-27 13:48:48 +0600

    post shown in the ui is complete

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,53 @@
-import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Grid, Loader } from "semantic-ui-react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  // console.log(data);
+
+  if (loading) {
+    return <Loader active inline="centered" />;
+  }
+
+  return (
+    <Grid columns={3} divided>
+      <Grid.Row>
+        <h2>Recent Posts</h2>
+      </Grid.Row>
+      <Grid.Row>
+        {data?.getPosts &&
+          data?.getPosts?.map((post) => (
+            <Grid.Column key={post.id}>
+              <h3>{post.body}</h3>
+            </Grid.Column>
+          ))}
+      </Grid.Row>
+    </Grid>
+  );
 };
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      comments {
+        id
+        username
+        body
+        createdAt
+      }
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      commentCount
+    }
+  }
+`;
 
 export default Home;

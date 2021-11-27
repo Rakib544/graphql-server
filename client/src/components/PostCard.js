8ac72aafeb1b57019b9f436f737995599d7f2ca1
Rakib544 <md.rakib10122003@gmail.com>
2021-11-27 19:51:33 +0600

    post card created

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,34 @@
+import { Button, Card, Image } from "semantic-ui-react";
+
+const PostCard = ({
+  post: { username, createdAt, body, likeCount, commentCount, likes },
+}) => {
+  return (
+    <Card>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta>Friends of Elliot</Card.Meta>
+        <Card.Description>
+          Steve wants to add you to the group <strong>best friends</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className="ui two buttons">
+          <Button basic color="green">
+            Approve
+          </Button>
+          <Button basic color="red">
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default PostCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import { Grid, Loader } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
 
 const Home = () => {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
@@ -18,7 +19,7 @@ const Home = () => {
         {data?.getPosts &&
           data?.getPosts?.map((post) => (
             <Grid.Column key={post.id}>
-              <h3>{post.body}</h3>
+              <PostCard post={post} />
             </Grid.Column>
           ))}
       </Grid.Row>

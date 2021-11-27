5229a5d9ef4cae688235d1c5cfa72fd47a44125a
Rakib544 <md.rakib10122003@gmail.com>
2021-11-27 13:09:07 +0600

    routing setup complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

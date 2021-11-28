8e365e7c0f679a09e89b287d5c909038599de529
Rakib544 <md.rakib10122003@gmail.com>
2021-11-28 15:12:25 +0600

    delete button compoent created

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,7 +1,13 @@
 import { gql, useQuery } from "@apollo/client";
+import moment from "moment";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { Button, Card, Grid, Icon, Image, Label } from "semantic-ui-react";
+import LikeButton from "../components/LikeButton";
+import { AuthContext } from "../context/auth";
 
 const SinglePost = () => {
+  const { user } = useContext(AuthContext);
   const { id } = useParams();
 
   const { data, loading } = useQuery(FETCH_POST_QUERY, {
@@ -10,13 +16,56 @@ const SinglePost = () => {
     },
   });
 
-  console.log(data);
+  const {
+    body,
+    createdAt,
+    username,
+    likes,
+    comments,
+    likeCount,
+    commentCount,
+  } = data?.getPost;
 
   if (loading) {
     return <h1>Loading...</h1>;
   }
 
-  return <div>Hello world</div>;
+  return (
+    <Grid.Row>
+      <Grid.Column width={2}>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        />
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>{username}</Card.Header>
+            <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+            <Card.Description>{body}</Card.Description>
+          </Card.Content>
+          <hr />
+          <Card.Content extra>
+            <LikeButton user={user} post={{ id, likes, likeCount }} />
+            <Button
+              as="div"
+              labelPosition="right"
+              onClick={() => console.log("rakib")}
+            >
+              <Button basic color="blue">
+                <Icon name="comments" />
+              </Button>
+              <Label basic color="blue" pointing="left">
+                {commentCount}
+              </Label>
+            </Button>
+          </Card.Content>
+        </Card>
+      </Grid.Column>
+    </Grid.Row>
+  );
 };
 
 const FETCH_POST_QUERY = gql`

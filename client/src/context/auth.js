acf5cbf88b64fe80f1936c5535a3c0c86d49d085
Rakib544 <md.rakib10122003@gmail.com>
2021-11-28 10:54:23 +0600

    nerw commit

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,4 +1,17 @@
+import jwtDecode from "jwt-decode";
 import { createContext, useReducer } from "react";
+
+const initialState = { user: null };
+
+if (localStorage.getItem("jwtToken")) {
+  const token = jwtDecode(localStorage.getItem("jwtToken"));
+
+  if (token.exp * 1000 < new Date().getTime()) {
+    localStorage.removeItem("jwtToken");
+  } else {
+    initialState.user = token;
+  }
+}
 
 const AuthContext = createContext({
   user: null,
@@ -24,9 +37,10 @@ function authReducer(state, action) {
 }
 
 function AuthProvider(props) {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
   const login = (userInfo) => {
+    localStorage.setItem("jwtToken", userInfo.token);
     dispatch({
       type: "LOGIN",
       payload: userInfo,
@@ -34,6 +48,7 @@ function AuthProvider(props) {
   };
 
   const logout = () => {
+    localStorage.removeItem("jwtToken");
     dispatch({
       type: "LOGOUT",
     });
